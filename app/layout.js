90cf3beee3f459ebbcb417fc7aa24345a8c38ec0
Mismail9961 <ismailarshad99699@gmail.com>
2025-10-01 07:57:52 -0700

    env file and paths are changes for secuirty

--- app/layout.js
+++ app/layout.js
@@ -2,23 +2,27 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
-
-const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+import { ClerkProvider } from "@clerk/nextjs";
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+  display: "swap", // better performance
+});
 
 export const metadata = {
   title: "QuickCart - GreatStack",
-  description: "E-Commerce with Next.js ",
+  description: "E-Commerce with Next.js",
 };
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
       <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`} >
-          <Toaster />
-          <AppContextProvider>
-            {children}
-          </AppContextProvider>
+        <body className={`${outfit.className} antialiased text-gray-700`}>
+          <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+          <AppContextProvider>{children}</AppContextProvider>
         </body>
       </html>
+    </ClerkProvider>
   );
 }
